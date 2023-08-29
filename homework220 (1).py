cb3c2b27b9591b5e3f9fa8923869245dea707898
n9967 <143535191+n9967@users.noreply.github.com>
2023-08-29 13:34:51 -0400

    Add files via upload

---
+++ homework220 (1).py
@@ -0,0 +1,10 @@
+#Nooreddin Hellalat SDEV220
+seconds_in_hour = 60 * 60
+print (seconds_in_hour)
+seconds_in_day = seconds_in_hour * 24
+print (seconds_in_day)
+result1 = seconds_in_day / seconds_in_hour
+print (result1)
+result2 = seconds_in_day // seconds_in_hour
+print (result2)
+#this does not agree with the original value and has made the result strictly an integer
